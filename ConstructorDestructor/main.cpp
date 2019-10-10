d01126af10e2f1ff478471734281df23e9bfab46
Киричёк Олег <okirichek@AVS.local>
2019-10-10 16:15:14 +0300

    Final version

--- ConstructorDestructor/main.cpp
+++ ConstructorDestructor/main.cpp
@@ -1,16 +1,40 @@
 #include <iostream>
 #include <vector>
 
+/*
+ * explicit не позволит конструкцию вида A obj2 = obj1 , она говорит "вызывайте конструктор копирования"
+*/
+
+/*
+ * Default ctor
+Default ctor
+Copy ctor
+Copy ctor
+Copy ctor
+Destructor
+Hello World!
+Destructor
+Destructor
+Destructor
+Destructor
+вывод от emplace_back не отличается
+*/
+
 using namespace std;
 
 class A
 {
 public:
-    A() { cout << "Default ctor" << endl; }
-    A(const A& a) { cout << "Copy ctor" << endl; }
+    explicit A() { cout << "Default ctor" << endl; }
+    explicit A(const A& a) { cout << "Copy ctor" << endl; }
+    explicit A(const A&& a) { cout << "Move ctor" << endl; }
     ~A() { cout << "Destructor" << endl; }
     A& operator=(const A& a) {
         cout << "Operator =" << endl;
+        return *this;
+    }
+    A& operator=(const A&& a) {
+        cout << "Move Operator =" << endl;
         return *this;
     }
 };
@@ -20,7 +44,9 @@ int main()
     vector<A> vectorA(1);
     A obj1;
     A obj2(obj1);
-    vectorA.push_back(obj2);
+    vectorA.emplace_back(obj2);
     cout << "Hello World!" << endl;
     return 0;
 }
+
+
